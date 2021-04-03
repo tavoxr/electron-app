86f167c8c6753620df53f51ba022b15ea89148a4
tavoxr <tavoxr23@gmail.com>
2021-04-03 06:13:15 -0600

    orders

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-const {app, BrowserWindow, ipcMain} = require('electron')
+const { app, BrowserWindow, ipcMain } = require('electron')
 const path = require('path')
-const mysql =  require('mysql2')
+const mysql = require('mysql2')
 const { create } = require('domain')
 
-if(process.env.NODE_ENV !== 'production'){
-    require('electron-reload')(__dirname,{
+if (process.env.NODE_ENV !== 'production') {
+    require('electron-reload')(__dirname, {
         electron: path.join(__dirname, 'node_modules', 'bin', 'electron')
     })
 
@@ -19,11 +19,11 @@ let orderFormWindow
 let ordersWindow
 let registerWindow
 
-function createMainWindow(){
+function createMainWindow() {
     mainWindow = new BrowserWindow({
-        width:800,
-        height:600,
-        webPreferences:{
+        width: 800,
+        height: 600,
+        webPreferences: {
             // nodeIntegration: true,
             // contextIsolation: true,
             preload: path.join(__dirname, 'preload.js')
@@ -33,14 +33,14 @@ function createMainWindow(){
     mainWindow.loadFile('index.html')
 }
 
-app.whenReady().then(()=>{
+app.whenReady().then(() => {
     createMainWindow()
     // createRegisterWindow()
     // createProductsWindow()
     // createProductFormWindow()
 
-    app.on('activate', ()=>{
-        if(BrowserWindow.getAllWindows().length === 0){
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
             createMainWindow()
         }
     })
@@ -50,8 +50,8 @@ app.whenReady().then(()=>{
     // })
 })
 
-app.on('window-all-closed',()=>{
-    if(process.platform !== 'darwin'){
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
         app.quit()
     }
 })
@@ -60,11 +60,11 @@ app.on('window-all-closed',()=>{
 /* =======================================================================================================
                                                 CREATE WINDOWS 
 =========================================================================================================*/
-function createProductsWindow(){
-    productsWindow =  new BrowserWindow({
-        width:800,
-        height:600,
-        webPreferences:{
+function createProductsWindow() {
+    productsWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
             // nodeIntegration: true,
             // contextIsolation: true,
             preload: path.join(__dirname, 'preload.js')
@@ -74,11 +74,11 @@ function createProductsWindow(){
     productsWindow.loadFile('./src/templates/Products.html')
 }
 
-function createRegisterWindow(){
-    registerWindow =  new BrowserWindow({
-        width:600,
-        height:600,
-        webPreferences:{
+function createRegisterWindow() {
+    registerWindow = new BrowserWindow({
+        width: 600,
+        height: 600,
+        webPreferences: {
             // nodeIntegration: true,
             // contextIsolation: true,
             preload: path.join(__dirname, 'preload.js')
@@ -87,11 +87,11 @@ function createRegisterWindow(){
 
     registerWindow.loadFile('./src/templates/Register.html')
 }
-function createProductFormWindow(){
-    productFormWindow =  new BrowserWindow({
-        width:600,
-        height:600,
-        webPreferences:{
+function createProductFormWindow() {
+    productFormWindow = new BrowserWindow({
+        width: 600,
+        height: 600,
+        webPreferences: {
             // nodeIntegration: true,
             // contextIsolation: true,
             preload: path.join(__dirname, 'preload.js')
@@ -101,11 +101,11 @@ function createProductFormWindow(){
     productFormWindow.loadFile('./src/templates/ProductForm.html')
 }
 
-function createOrderFormWindow(){
-    orderFormWindow =  new BrowserWindow({
-        width:600,
-        height:600,
-        webPreferences:{
+function createOrderFormWindow() {
+    orderFormWindow = new BrowserWindow({
+        width: 600,
+        height: 600,
+        webPreferences: {
             // nodeIntegration: true,
             // contextIsolation: true,
             preload: path.join(__dirname, 'preload.js')
@@ -115,11 +115,11 @@ function createOrderFormWindow(){
     orderFormWindow.loadFile('./src/templates/OrderForm.html')
 }
 
-function createOrdersWindow(){
-    ordersWindow =  new BrowserWindow({
-        width:800,
-        height:600,
-        webPreferences:{
+function createOrdersWindow() {
+    ordersWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
             // nodeIntegration: true,
             // contextIsolation: true,
             preload: path.join(__dirname, 'preload.js')
@@ -136,165 +136,223 @@ function createOrdersWindow(){
 /*==========================================================================================================
                                                 IPCMAIN                                            
 ============================================================================================================*/
-ipcMain.on('saludo',(e,msg)=>{
+ipcMain.on('saludo', (e, msg) => {
 
-    productsWindow.webContents.on('did-finish-load',()=>{
+    productsWindow.webContents.on('did-finish-load', () => {
         productsWindow.webContents.send('envioSaludo', msg)
     })
 })
 
 
 var userEmpleado
-ipcMain.on('loginUser',(e,userIdEmpleado)=>{
+ipcMain.on('loginUser', (e, userIdEmpleado) => {
     createProductsWindow()
 
     mainWindow.close()
     userEmpleado = userIdEmpleado
     console.log('employee', userIdEmpleado)
-  
 
-connection.promise().query(`SELECT * FROM Product WHERE idEmployee = ${userIdEmpleado};`)
-.then(([results,fields])=>{
-    console.log(results)
-    productsWindow.webContents.on('did-finish-load', function () {
-        productsWindow.webContents.send('getMyProducts', results);
-        
-    });
-}
 
-).catch((err)=>{
+    connection.promise().query(`SELECT * FROM Product WHERE idEmployee = ${userIdEmpleado};`)
+        .then(([results, fields]) => {
+            console.log(results)
+            productsWindow.webContents.on('did-finish-load', function () {
+                productsWindow.webContents.send('getMyProducts', results);
+
+            });
+        }
+
+        ).catch((err) => {
+
+        })
 
 })
- 
-})
 
-ipcMain.on('abrirFormProduct', (e,msg)=>{
+ipcMain.on('abrirFormProduct', (e, msg) => {
 
     createProductFormWindow()
 
-connection.promise().query(`SELECT * FROM Product WHERE idEmployee = ${userEmpleado};`)
-.then(([results,fields])=>{
-    console.log(results)
-    productFormWindow.webContents.on('did-finish-load', function () {
-        productFormWindow.webContents.send('getMyProductsValidation', results);
-        
-    });
-}
+    connection.promise().query(`SELECT * FROM Product WHERE idEmployee = ${userEmpleado};`)
+        .then(([results, fields]) => {
+            console.log(results)
+            productFormWindow.webContents.on('did-finish-load', function () {
+                productFormWindow.webContents.send('getMyProductsValidation', results);
 
-).catch((err)=>{
+            });
+        }
+
+        ).catch((err) => {
+
+        })
 
 })
 
-})
-
-ipcMain.on('cerrarFormProduct', (e,window)=>{
+ipcMain.on('cerrarFormProduct', (e, window) => {
 
     productFormWindow.close()
 })
 
-ipcMain.on('abrirOrderForm', (e,msg)=>{
+ipcMain.on('abrirOrderForm', (e, msg) => {
     createOrderFormWindow()
 })
 
-ipcMain.on('cerrarOrderForm',(e,msg)=>{
+ipcMain.on('cerrarOrderForm', (e, msg) => {
     orderFormWindow.close()
 })
 
 
-ipcMain.on('redirigirAOrders', (e,window)=>{
+ipcMain.on('redirigirAOrders', (e, window) => {
 
-    if(window != 'Orders'){
+    if (window != 'Orders') {
         createOrdersWindow()
         productsWindow.close()
+
+        connection.promise().query(`SELECT * FROM Orders WHERE idEmployee = ${userEmpleado};`)
+            .then(([results, fields]) => {
+                console.log(results)
+                ordersWindow.webContents.on('did-finish-load', function () {
+                    ordersWindow.webContents.send('getAllOrders', results);
+
+                });
+            }
+
+            ).catch((err) => {
+
+            })
     }
+
+
+
+
+    connection.promise().query(`SELECT * FROM Product WHERE idEmployee = ${userEmpleado};`)
+        .then(([results, fields]) => {
+            console.log(results)
+            productsWindow.webContents.on('did-finish-load', function () {
+                productsWindow.webContents.send('getMyProducts', results);
+
+            });
+        }
+
+        ).catch((err) => {
+
+        })
+
+
+
 })
 
-ipcMain.on('redirigirAProducts',(e,window)=>{
-    if(window != 'Products'){
+ipcMain.on('redirigirAProducts', (e, window) => {
+    if (window != 'Products') {
         createProductsWindow()
-        ordersWindow.close()        
+        ordersWindow.close()
+
+
+        connection.promise().query(`SELECT * FROM Product WHERE idEmployee = ${userEmpleado};`)
+            .then(([results, fields]) => {
+                console.log(results)
+                productsWindow.webContents.on('did-finish-load', function () {
+                    productsWindow.webContents.send('getMyProducts', results);
+
+                });
+            }
+
+            ).catch((err) => {
+
+            })
     }
+    connection.promise().query(`SELECT * FROM Orders WHERE idEmployee = ${userEmpleado};`)
+        .then(([results, fields]) => {
+            console.log(results)
+            ordersWindow.webContents.on('did-finish-load', function () {
+                ordersWindow.webContents.send('getAllOrders', results);
+
+            });
+        }
+
+        ).catch((err) => {
+
+        })
 })
 
-ipcMain.on('registerToProducts',(e, msg)=>{
+ipcMain.on('registerToProducts', (e, msg) => {
     createProductsWindow()
     registerWindow.close()
 })
 
-ipcMain.on('enviarAlLogin',(e, msg)=>{
+ipcMain.on('enviarAlLogin', (e, msg) => {
     createMainWindow()
     registerWindow.close()
 
-connection.promise().query('SELECT * FROM User;')
-.then(([results,fields])=>{
-    console.log(results)
-    mainWindow.webContents.on('did-finish-load', function () {
-        mainWindow.webContents.send('getUsuariosLogin', results);
-        
-    });
+    connection.promise().query('SELECT * FROM User;')
+        .then(([results, fields]) => {
+            console.log(results)
+            mainWindow.webContents.on('did-finish-load', function () {
+                mainWindow.webContents.send('getUsuariosLogin', results);
 
-   
+            });
 
 
-    // registerWindow.webContents.send('getUsuarios', results)
-}
 
-).catch((err)=>{
+
+            // registerWindow.webContents.send('getUsuarios', results)
+        }
+
+        ).catch((err) => {
+
+        })
+
 
 })
 
-    
-})
-
-ipcMain.on('returnRegisterToLogin',(e,msg)=>{
+ipcMain.on('returnRegisterToLogin', (e, msg) => {
     createMainWindow()
     registerWindow.close()
 
-connection.promise().query('SELECT * FROM User;')
-.then(([results,fields])=>{
-    console.log(results)
-    mainWindow.webContents.on('did-finish-load', function () {
-        mainWindow.webContents.send('getUsuariosLogin', results);
-        
-    });
+    connection.promise().query('SELECT * FROM User;')
+        .then(([results, fields]) => {
+            console.log(results)
+            mainWindow.webContents.on('did-finish-load', function () {
+                mainWindow.webContents.send('getUsuariosLogin', results);
 
-   
+            });
 
 
-    // registerWindow.webContents.send('getUsuarios', results)
-}
 
-).catch((err)=>{
+
+            // registerWindow.webContents.send('getUsuarios', results)
+        }
+
+        ).catch((err) => {
+
+        })
 
 })
 
-})
 
-
-ipcMain.on('returnLoginToRegister',(e,msg)=>{
+ipcMain.on('returnLoginToRegister', (e, msg) => {
     createRegisterWindow()
     mainWindow.close()
 
-connection.promise().query('SELECT * FROM User;')
-.then(([results,fields])=>{
-    console.log(results)
-    registerWindow.webContents.on('did-finish-load', function () {
-        registerWindow.webContents.send('getUsuarios', results);
-        
-    });
+    connection.promise().query('SELECT * FROM User;')
+        .then(([results, fields]) => {
+            console.log(results)
+            registerWindow.webContents.on('did-finish-load', function () {
+                registerWindow.webContents.send('getUsuarios', results);
 
-   
+            });
 
 
-    // registerWindow.webContents.send('getUsuarios', results)
-}
 
-).catch((err)=>{
+
+            // registerWindow.webContents.send('getUsuarios', results)
+        }
+
+        ).catch((err) => {
+
+        })
 
 })
-    
-})
-const  connection =  mysql.createConnection({
+const connection = mysql.createConnection({
     user: 'root',
     host: 'localhost',
     password: 'evangelion01',
@@ -303,48 +361,49 @@ const  connection =  mysql.createConnection({
 })
 
 
-ipcMain.on('registrarUsuario',(e,employee)=>{
+ipcMain.on('registrarUsuario', (e, employee) => {
 
     console.log('employee', employee)
-    connection.query(`INSERT INTO User(idEmployee,name,password) VALUES(${employee.idEmployee},"${employee.name}","${employee.password}");`,(err,results,fields)=>{
-            console.log('USER CREATED')
-        })
+    connection.query(`INSERT INTO User(idEmployee,name,password) VALUES(${employee.idEmployee},"${employee.name}","${employee.password}");`, (err, results, fields) => {
+        console.log('USER CREATED')
+    })
 })
 
 
 // ipcMain.on('getMyProductsValidation', (e,msg)=>{
 
-    
+
 
 // })
 
-ipcMain.on('registrarProducto',(e,product)=>{
+ipcMain.on('registrarProducto', (e, product) => {
 
     console.log('employee', product)
-    connection.query(`INSERT INTO Product(name,description, price, inStock, category,idEmployee) VALUES("${product.name}","${product.description}",${product.price},"${product.inStock}","${product.category}",${userEmpleado});`,(err,results,fields)=>{
-            console.log('PRODUCT CREATED')
-        })
+    connection.query(`INSERT INTO Product(name,description, price, inStock, category,idEmployee) VALUES("${product.name}","${product.description}",${product.price},"${product.inStock}","${product.category}",${userEmpleado});`, (err, results, fields) => {
+        console.log('PRODUCT CREATED')
+    })
 })
 
-ipcMain.on('enviarProductsDesdeElForm', (e, msg)=>{
+ipcMain.on('enviarProductsDesdeElForm', (e, msg) => {
     productsWindow.close()
-    createProductsWindow()    
+    createProductsWindow()
     productFormWindow.close()
 
 
-connection.promise().query(`SELECT * FROM Product WHERE idEmployee = ${userEmpleado};`)
-.then(([results,fields])=>{
-    console.log(results)
-    productsWindow.webContents.on('did-finish-load', function () {
-        productsWindow.webContents.send('getMyProducts', results);
-        
-    });
-}
+    connection.promise().query(`SELECT * FROM Product WHERE idEmployee = ${userEmpleado};`)
+        .then(([results, fields]) => {
+            console.log(results)
+            productsWindow.webContents.on('did-finish-load', function () {
+                productsWindow.webContents.send('getMyProducts', results);
 
-).catch((err)=>{
+            });
+        }
 
+        ).catch((err) => {
+
+        })
 })
-})
+
 
 /*==========================================================================================================
                                              END IPCMAIN                                            
@@ -363,13 +422,13 @@ connection.promise().query(`SELECT * FROM Product WHERE idEmployee = ${userEmple
 //     console.log(results)
 //     registerWindow.webContents.on('did-finish-load', function () {
 //         registerWindow.webContents.send('getUsuarios', results);
-        
+
 //     });
 
-   
 
 
-    // registerWindow.webContents.send('getUsuarios', results)
+
+// registerWindow.webContents.send('getUsuarios', results)
 // }
 
 // ).catch((err)=>{
@@ -394,24 +453,24 @@ connection.promise().query(`SELECT * FROM Product WHERE idEmployee = ${userEmple
 
 
 connection.promise().query('SELECT * FROM User;')
-.then(([results,fields])=>{
-    console.log(results)
-    mainWindow.webContents.on('did-finish-load', function () {
-        mainWindow.webContents.send('getUsuariosLogin', results);
-        
-    });
+    .then(([results, fields]) => {
+        console.log(results)
+        mainWindow.webContents.on('did-finish-load', function () {
+            mainWindow.webContents.send('getUsuariosLogin', results);
 
-   
+        });
 
 
-    // registerWindow.webContents.send('getUsuarios', results)
-}
 
-).catch((err)=>{
 
-})
+        // registerWindow.webContents.send('getUsuarios', results)
+    }
+
+    ).catch((err) => {
+
+    })
 
 
 /*==========================================================================================================
-                                             END CONEXION MYSQL                                          
+                                             END CONEXION MYSQL
 ============================================================================================================*/
--- src/js/orders.js
+++ src/js/orders.js
@@ -1,6 +1,8 @@
 const myProducts  = document.querySelector('#myProducts')
 const myOrders  = document.querySelector('#myOrders')
 const btnPlaceOrder  = document.querySelector('#btn-placeOrder')
+const tableBody = document.querySelector('#table-body')
+
 
 const thisWindow = 'Orders'
 
@@ -17,3 +19,27 @@ btnPlaceOrder.addEventListener('click',()=>{
     window.api.send('abrirOrderForm','abrir')
 })
 
+
+window.addEventListener("DOMContentLoaded",()=>{
+    //  console.log(window.api.saludo)
+
+
+    window.api.receive('getAllOrders',function(e,data) {
+
+
+        for(var i=0; i <data.length; i++){
+            let row = `<tr>
+                            <td>${data[i].id}</td>
+                            <td>${data[i].idProduct}</td>
+                            <td>${data[i].idEmployee}</td>
+                            <td>${data[i].quantity}</td>
+                       <tr/>`
+        
+                tableBody.innerHTML +=row
+            
+        }
+        
+
+    })
+
+})
